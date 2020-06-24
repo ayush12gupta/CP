8f3cc56465d43568a5cc1ce4b7393755618b0bd2
Ayush Gupta <56562280+ayush12gupta@users.noreply.github.com>
2020-06-24 13:33:31 +0530

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -10,3 +10,25 @@ const int N = 2e5 + 5;
 int n;
 int a[N];
 
+---------------------------------------------------------------------------------
+ 
+ #include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+typedef long long int ll;
+#define pb          push_back
+#define mp 			make_pair
+#define pii         pair<ll, ll>
+#define vi          vector<ll>
+#define vii         vector<pii>
+#define mi          map<ll,ll>
+#define mii         map<pii,ll>
+#define all(a)      (a).begin(),(a).end()
+#define Set(x, val) memset(x, val, siindeof(x))
+#define rep(i,a,b)  for(ll i=a;i<b;i++)
+#define ff 			first
+#define ss 			second 
+#define endl        '\n'
+const ll INF=1e18
